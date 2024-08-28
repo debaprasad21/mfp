d9229c76c55856f8f70ec2f036c8092f0b66d0e2
Debaprasad Sahoo <debaprasadworld@gmail.com>
2024-08-28 14:49:54 +0530

    [workflow]: Created container deploy script

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -2,6 +2,7 @@ import { mount } from "marketing/MarketingApp";
 import React, { useEffect, useRef } from "react";
 
 export default () => {
+  console.log("MarketingApp Loaded");
   const ref = useRef(null);
 
   useEffect(() => {
